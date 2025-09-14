c88e0b3f14b56f42643fd95638691cc5aa9e3c0e
ailton.fidelix <ailton1626@gmail.com>
2025-09-14 20:16:13 -0300

    feat: Add Qt examples

---
+++ examples/hello/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "Hello from Qt6 cross-compiling...";
+
+    return a.exec();
+}
